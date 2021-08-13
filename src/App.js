f36f77d178cec865c891489fb991f6f851a1f21d
Anthony-Batista <a.batista0321@gmail.com>
2021-08-13 17:18:55 -0400

    mvp

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import './App.css'
+import Axios from 'axios'
+import Character from './components/Character'
+import styled from 'styled-components'
+
+const Style = styled.div`
+	display: flex;
+	flex-direction: column;
+`
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+	const [people, setPeople] = useState([])
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+	// Try to think through what state you'll need for this app before starting. Then build out
+	// the state properties here.
 
-  return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
-  );
+	// Fetch characters from the API in an effect hook. Remember, anytime you have a
+	// side effect in a component, you want to think about which state and/or props it should
+	// sync up with, if any.
+
+	useEffect(() => {
+		Axios.get('https://swapi.dev/api/people/')
+			.then((r) => {
+				setPeople(r.data)
+				console.log(r.data)
+			})
+
+			.catch((err) => {
+				console.log(err)
+			})
+	}, [])
+
+	return (
+		<Style className="App">
+			<h1 className="Header">React Wars</h1>
+			{people.map((obj) => {
+				return <Character key={obj.name} starwars={obj} />
+			})}
+		</Style>
+	)
 }
 
-export default App;
+export default App
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,33 @@
 // Write your Character component here
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledCharacters = styled.div`
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	align-items: center;
+	padding: 1%;
+	border: 0.3rem solid gray
+	margin: 1rem 10%;
+	color: white;
+	font-family: 'Cinzel', serif;
+`
+const StyledBirth = styled.div`
+	color: #301934;
+	background-color: rgba(50, 50, 50, 0.5);
+	width: 4rem;
+	padding: 1rem;
+	border-radius: 1rem 2rem;
+`
+
+export default function Character(props) {
+	const { starwars } = props
+
+	return (
+		<StyledCharacters>
+			<h2>{starwars.name}</h2>
+			<StyledBirth> {starwars.birth_year} </StyledBirth>
+		</StyledCharacters>
+	)
+}
